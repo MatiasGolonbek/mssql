736aa0c673dc3d6b58d3699ed48c483f7e5042ab
MATIAS GOLONBEK <47343179@ort>
2023-05-22 09:08:04 -0300

    jsdjs

--- public/script.js
+++ public/script.js
@@ -43,13 +43,15 @@ function CargarTodo() {
                     <div class="colorCard card border-success mb-3 cardPokemon" style="max-width: 20rem;">
                       <div class="card-header bg-colorCard headCard  ">${item.Id}.  ${item.Nombre} </div>
                        <div class="card-body text-success card-group">
-                       <p>Importe: $${item.Importe}</p>
+                       <p class="firdo">Importe: $${item.Importe}</p>
                        <p>Descripcion: ${item.Descripcion}</p>
                        <p>Gluten: ${Gluten}</p>
                   </div>
                   <div class="card-footer bg-transparent border-success"> 
+                  <button class="btn boton" onclick="Borrar(${item.Id})">Eliminar Pizzeta</button>
                   </div>
                         </div>
+                        
                       </div>
                     </div>
                 </div>
@@ -66,11 +68,28 @@ function InsertPizza() {
   let gluten = document.getElementById("gluten").value
   let importe = document.getElementById("importe").value
   let descripcion = document.getElementById("descripcion").value
-  console.log("http://localhost:3000/insert?nombre=" + nombre + "&glutenFree=" + gluten + "&importe" + importe + "&descripcion" + descripcion)
+  console.log("http://localhost:3000/delete?id=" + nombre + "&glutenFree=" + gluten + "&importe" + importe + "&descripcion" + descripcion)
   axios
       .post("http://localhost:3000/insert?nombre=" + nombre + "&glutenFree=" + gluten + "&importe" + importe + "&descripcion" + descripcion)
       .then((result) => {
           console.log(result.data)
+          CargarTodo()
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+}
+
+function Borrar(id) {
+  let nombre = document.getElementById("nombre")
+  //let id = document.getElementById("id")
+  console.log("http://localhost:3000/api/pizzas/" + id)
+  axios
+      .delete("http://localhost:3000/api/pizzas/" + id)
+      .then((result) => {
+          console.log(result.data)
+          CargarTodo()
+          
       })
       .catch((error) => {
           console.log(error);
